825c508cb06cc1e67ede9930c6f6918e7d7d0e8a
Nitish Ragoomundun <lrugratz@gmail.com>
2022-09-03 17:00:42 +0400

    Updated PhDComics_dates_titles.txt and download scripts to include comic 2050

--- PhDComics_download_v1.1.py
+++ PhDComics_download_v1.1.py
@@ -39,8 +39,8 @@ from pathlib import Path
 
 #  Range of indices of the comic strips which have to be
 #  downloaded.
-startIdx = 2042
-endIdx   = 2042
+startIdx = 1
+endIdx   = 2050
 
 #  Base link of all the PhD Comics pages
 BaseUrl = "http://phdcomics.com/comics/archive.php?comicid="
@@ -63,7 +63,7 @@ anomalies = [  191,  194,  574,  657, 1433, 1489, 1513, 1519, 1522, 1524,
               1726, 1748, 1766, 1769, 1770, 1777, 1788, 1805, 1845, 1853,
               1855, 1864, 1874, 1880, 1886, 1889, 1902, 1931, 1933, 1935,
               1939, 1949, 1954, 1959, 1964, 1985, 2009, 2025, 2033, 2041,
-              2047]
+              2047, 2050]
 
 
 
